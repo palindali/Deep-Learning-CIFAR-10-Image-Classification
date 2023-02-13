6c30083fd88736b242826cb13ceb18bd3ca7cadf
Ali El-Said <palindali@gmail.com>
2023-02-13 17:03:29 -0500

    new test label from usuf 73 submission

--- preprocess_data.py
+++ preprocess_data.py
@@ -29,12 +29,12 @@ labels['actual_label'] = labels['label'].map(mapping)
 
 label_list = np.sort(labels['label'].unique())
 
-for label in label_list:
+for label in tqdm(label_list):
     images = (labels.loc[labels['label'] == label,'id'].astype('string') + '.jpg').tolist()
     
-    new_dir = f'./data/{name}/' + mapping[label]
+    new_dir = f'./data/{name}_new/' + mapping[label]
     os.makedirs(new_dir, exist_ok=True)
-    for image in tqdm(images, str(label)):
+    for image in tqdm(images, str(label), leave=False):
         old_path = f'./data/{name}_bkp/{name}/' + image
         new_path = new_dir + '/' + image
         shutil.copy(old_path, new_path)
