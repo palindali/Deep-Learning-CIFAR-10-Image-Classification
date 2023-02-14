c552ff55a9f8104aab7ba0e14deb23395ccf24da
Ali El-Said <palindali@gmail.com>
2023-02-13 20:32:49 -0500

    voting best classifier outputs into one

--- voter.py
+++ voter.py
@@ -7,6 +7,7 @@ statistics.mode([3,2,1])
 
 
 s1 = pd.read_csv('./stuff/voting/sub1.csv')
+# s11 = pd.read_csv('./stuff/voting/sub11.csv')
 s2 = pd.read_csv('./stuff/voting/sub2.csv')
 s3 = pd.read_csv('./stuff/voting/sub3.csv')
 
@@ -18,6 +19,8 @@ df = pd.DataFrame({
 
 s_voted = df.apply(statistics.mode, axis=1)
 
-s1['label'] = s_voted
+sum( (s1['label'] - s2['label']) == 0) / 100
+sum( (s11['label'] - s_voted) == 0) / 100
 
+s1['label'] = s_voted
 s1.to_csv('./stuff/voting/submission_voted.csv', index=False)
